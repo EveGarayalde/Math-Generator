42c34eaf6edba86588deae2447c9988f57965490
EveGarayalde <106777418+EveGarayalde@users.noreply.github.com>
2022-06-03 12:36:23 -0400

    Update Generator.java

--- Generator.java
+++ Generator.java
@@ -1,23 +1,31 @@
 package mod;
 
 public class Generator {
-
+	
+	/*These instance variables are used to store data for the number of correct and incorrect questions that have been answered,
+	  along with the correct answer for the current question.*/
 	private int correct;
 	private int incorrect;
 	private int answer;
 	
+	//These methods give access to the values that are stored within the private instance variables.
 	public int getCorrect() {return correct;}
 	public int getIncorrect() {return incorrect;}
 	public int getAnswer() {return answer;}
 	
+	//These setters allow for the incrementation of the instance varaibles from outside classes.
 	public void addC() {correct++;}
 	public void addI() {incorrect++;}
 	
+	//The constructor sets the "correct" and "incorrect" instance variables to be 0, as no questions have been answered yet.
 	public Generator() {
 		correct = 0;
 		incorrect = 0;
 	}
 	
+	/*This method is used to generate a new addition problem. It will create the two numbers from 0 - 100. The answer to the question is
+	  the greater of the two numbers, and is stored in the "answer" instance variable. It will return the finished equation, which is
+	  (bigger number - smaller number) + smaller number = bigger number.*/
 	public String generateAddition() {
 		int num1 = ((int) (Math.random() * 10)) + (((int) (Math.random() * 10)) * 10);
 		if(Math.random() < 0.50)
@@ -35,6 +43,8 @@ public class Generator {
 		}
 	}
 	
+	/*This method is used to generate a new subtraction problem. The method generates two int variables with random values. It will determine which value
+	  is greater, and set it as the answer to the equation. The returned equation is (bigger number + smaller number) - (smaller number) = (bigger number).*/
 	public String generateSubtraction() {
 		int num1 = ((int) (Math.random() * 10)) + (((int) (Math.random() * 10)) * 10);
 		if(Math.random() < 0.50)
@@ -52,6 +62,9 @@ public class Generator {
 		}
 	}
 	
+	/*This method is used to generate a new multiplication problem. The method creates two random int variables, one with a value from 0-100, and the second
+	  with a value from 0-10. The first variable is the answer to the equation. The second variable is used to divide the first variable to find the other
+	  factor in the equation. The returned equation is (bigger number / smaller number) * (smaller number) = (bigger number).*/
 	public String generateMultiplication() {
 		int num1 = ((int) (Math.random() * 10)) + (((int) (Math.random() * 10)) * 10);
 		if(Math.random() < 0.50)
@@ -65,6 +78,9 @@ public class Generator {
 		return answer / num2 + " \u00D7 " + num2 + " = ";
 	}
 	
+	/*This method is used to generate a new division problem. The method creates two random int variables with values from 0-100. The first int generated is used
+	  as the answer to the equation. The dividend of the equation is found by multiplying the two random ints together. The returned equation is 
+	  (number1 * number2) / (number2) = (number1).*/
 	public String generateDivision() {
 		int num1 = ((int) (Math.random() * 10)) + (((int) (Math.random() * 10)) * 10);
 		if(Math.random() < 0.50)
@@ -76,6 +92,7 @@ public class Generator {
 		return (num1 * num2) + " \u00F7 " + num2 + " = ";
 	}
 	
+	//This method will choose a random math problem to generate each time it is called. 
 	public String generateRandom() {
 		double x = Math.random();
 		if(x < 0.25) {
